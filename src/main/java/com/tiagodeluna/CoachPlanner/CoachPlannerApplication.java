890b72a87642cd3a45297f6aeac0f5c3e77a0021
Tiago Luna <tiagodeluna@hotmail.com>
2021-08-12 11:57:35 +0200

    Initial commit

---
+++ src/main/java/com/tiagodeluna/CoachPlanner/CoachPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.tiagodeluna.CoachPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachPlannerApplication.class, args);
+	}
+
+}
